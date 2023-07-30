d09677adb4a37eb0880baf82e365ebc3868d4b34
Harman1107 <2020kuec2001@iiitkota.ac.in>
2023-07-30 22:22:06 +0530

    initial commit

---
+++ client/src/components/Home/HeaderComp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HeaderComp.css';
+
+function HeaderComp() {
+  return (
+    <div className="HeaderComp">
+      <h1>Trading App</h1>
+    </div>
+  )
+}
+
+export default HeaderComp
